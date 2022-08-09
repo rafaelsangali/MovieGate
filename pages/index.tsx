fd4e16bd7febb2e86983cf32b193ae8b2d933cb0
Rafael Sangali <rafaelsangali@outlook.com>
2022-08-09 16:57:44 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Organizze</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+      <main>
+        <h1 className='bg-blue-700'>Hello World</h1>
+      </main>
+    </div>
+  );
+}
