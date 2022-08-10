f4d87ce8161e3bc667908ab39d5b6cf701a42f57
Marcus Vinicius <marcusv.ribas7@gmail.com>
2022-08-10 14:42:44 -0300

    🐛 FIX: changed code to eslint's pattern

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from 'next/head'
 
 export default function Home() {
   return (
@@ -8,8 +8,8 @@ export default function Home() {
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <main>
-        <h1 className='bg-blue-700'>Hello World</h1>
+        <h1 className="bg-blue-700">Hello World</h1>
       </main>
     </div>
-  );
+  )
 }
