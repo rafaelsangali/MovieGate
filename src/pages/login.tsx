f23b6b4fa6cee3a8fbf3f1945b020357cd741b0f
Marcus Vinicius <marcusv.ribas7@gmail.com>
2022-08-11 16:22:27 -0300

    👌 IMPROVE: improved semantic in home page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -9,28 +9,47 @@ export default function Home() {
         <title>SpendTracker</title>
         <link rel="icon" href={icons.iconFinance} />
       </Head>
-      <header className="bg-midnight-blue h-3/4 w-full top-0 absolute -z-10"></header>
-      <main className="h-screen grid justify-items-center gap-24 place-content-center">
+      <main className="flex flex-col pt-[18%] justify-center items-center gap-10 h-screen bg-midnight-blue">
         <a
-          className="sm:scale-125 md:scale-150 transition-transform"
-          href={'/'}
+          className="sm:scale-125 md:scale-150 transition-transform flex flex-col"
+          href="/"
         >
-          <>
-            <Image src={icons.iconFinance} width={110} alt="Icone OrganiEZ" />
-            <h1 className="text-white">SpendTracker</h1>
-          </>
+          <Image src={icons.iconFinance} width={110} alt="Icone SpendTracker" />
+          <h1 className="text-white text-xl">SpendTracker</h1>
         </a>
-        <h2 className="h-14 mx-10 text-center text-white font-medium text-lg sm:text-xl md:text-2xl">
+        <h2 className=" mx-10 text-center text-white font-medium text-3xl sm:text-xl md:text-2xl">
           Controle suas finanças de forma muito simples
         </h2>
-        <button className="flex items-center w-[312px] h-[56px] bg-white rounded">
-          <div className='px-4 h-full flex text-center border-r-2'>
+        <h3 className="text-white text-center w-[50%] mt-10 mb-[5%]">
+          Faça seu login com sua conta google
+        </h3>
+        {/* <button
+          type="button"
+          className="flex items-center gap-16 bg-white rounded py-[18px] px-4"
+        >
+          <div className="flex text-center border-r-2">
             <Image src={icons.iconGoogle} alt="Icone Google" />
           </div>
-          <p className='w-full'>Entrar com Google</p>
-        </button>
+          <p className="mr-10">Entrar com Google</p>
+        </button> */}
+
+        <div className="bg-slate-gray h-full w-screen">
+          <button
+            type="button"
+            className="flex gap-[12%] w-[80%] h-14 mx-auto items-center bg-white rounded translate-y-[-50%]"
+          >
+            <div className="flex border-r-2 p-2">
+              <Image
+                src={icons.iconGoogle}
+                alt="Icone Google"
+                className="border-rounded"
+              />
+            </div>
+            <p>Entrar com Google</p>
+          </button>
+        </div>
       </main>
-      <footer className="bg-slate-gray h-1/4 w-full bottom-0 absolute -z-10"></footer>
+      {/* <footer className="bg-slate-gray h-1/4 w-full bottom-0 absolute -z-10" /> */}
     </>
   )
 }
