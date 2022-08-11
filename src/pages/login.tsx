3ceb4f444cd81b9de4bc03c1a9749968db9fdd17
Marcus Vinicius <marcusv.ribas7@gmail.com>
2022-08-11 17:58:54 -0300

    👌 IMPROVE: semantic updates in login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -9,20 +9,26 @@ export default function Home() {
         <title>SpendTracker</title>
         <link rel="icon" href={icons.iconFinance} />
       </Head>
-      <main className="flex flex-col pt-[18%] justify-center items-center gap-10 h-screen bg-midnight-blue">
-        <a
-          className="sm:scale-125 md:scale-150 transition-transform flex flex-col"
-          href="/"
-        >
-          <Image src={icons.iconFinance} width={110} alt="Icone SpendTracker" />
-          <h1 className="text-white text-xl">SpendTracker</h1>
-        </a>
-        <h2 className=" mx-10 text-center text-white font-medium text-3xl sm:text-xl md:text-2xl">
-          Controle suas finanças de forma muito simples
-        </h2>
-        <h3 className="text-white text-center w-[50%] mt-10 mb-[5%]">
-          Faça seu login com sua conta google
-        </h3>
+      <main className="h-screen ">
+        <div className="bg-midnight-blue h-3/4 flex flex-col justify-center items-center ">
+          <a
+            className="sm:scale-125 md:scale-150 transition-transform flex flex-col"
+            href="/"
+          >
+            <Image
+              src={icons.iconFinance}
+              width={110}
+              alt="Icone SpendTracker"
+            />
+            <h1 className="text-white text-xl">SpendTracker</h1>
+          </a>
+          <h2 className="text-center mt-10 text-white font-medium text-3xl sm:text-xl md:text-2xl">
+            Controle suas finanças de forma muito simples
+          </h2>
+          <h3 className="text-white text-center w-1/2 mt-20">
+            Faça seu login com sua conta google
+          </h3>
+        </div>
         {/* <button
           type="button"
           className="flex items-center gap-16 bg-white rounded py-[18px] px-4"
@@ -33,10 +39,10 @@ export default function Home() {
           <p className="mr-10">Entrar com Google</p>
         </button> */}
 
-        <div className="bg-slate-gray h-full w-screen">
+        <div className="bg-slate-gray h-1/4 w-screen flex justify-center">
           <button
             type="button"
-            className="flex gap-[12%] w-[80%] h-14 mx-auto items-center bg-white rounded translate-y-[-50%]"
+            className="flex gap-[12%] w-4/5 h-14 items-center bg-white rounded -translate-y-1/2"
           >
             <div className="flex border-r-2 p-2">
               <Image
