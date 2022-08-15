fc27f6f2d5de1758eaa58a39d21d3a5eeef5c78b
Marcus Vinicius <marcusv.ribas7@gmail.com>
2022-08-15 15:51:55 -0300

    📦 NEW: add icon svg and start index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,33 @@
+import Image from 'next/image'
+import { icons } from '../../public'
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <main>
+      <div className="bg-midnight-blue flex flex-col">
+        <div className="flex items-center px-6 py-4 justify-between">
+          <div className="flex gap-4 items-center text-lg">
+            <div className="flex border-slate-gray border-2 w-12 h-12 rounded-full relative">
+              <Image
+                src={icons.iconFinance}
+                alt="Icone SpendTracker"
+                layout="fill"
+                className="rounded-full"
+              />
+            </div>
+            <p className="text-white">
+              Olá, <span className="font-bold">Marcus!</span>
+            </p>
+          </div>
+          <div className="flex ">
+            <Image
+              src={icons.iconPower}
+              alt="Icone Power"
+              className="rounded-full"
+            />
+          </div>
+        </div>
+      </div>
+    </main>
+  )
 }
