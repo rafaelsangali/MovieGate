0fab96cf2fb35b564e7dfcf020c0d0abfdf43284
Marcus Vinicius <marcusv.ribas7@gmail.com>
2022-08-16 15:47:35 -0300

    👌 IMPROVE: change index img to next's Image

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['https://lh3.googleusercontent.com/a-/']
+    domains: ['lh3.googleusercontent.com']
   }
 }
 
